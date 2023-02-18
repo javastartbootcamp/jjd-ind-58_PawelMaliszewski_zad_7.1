84877162789670d1a42cef10d56399908f5089a8
Pawel Maliszewski <pablo999455@gmail.com>
2023-02-18 17:34:29 +0100

    solution 2

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -4,18 +4,14 @@ public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        int count = 0;
-        if (array == null) {
+        if (array == null || array.length == 0) {
             return false;
         }
         for (int i = 0; i < array.length / 2; i++) {
-            if (array[i] == array[array.length - 1 - i]) {
-                count++;
-            }
-            if (count == array.length / 2) {
-                return true;
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
